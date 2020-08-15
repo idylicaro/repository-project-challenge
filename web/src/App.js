3923e0478c27b083ddf8246b29364f072bcba7f4
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-15 14:50:29 -0300

    feat: ensure that can to add new repository

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,41 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data); 
+    })
+  },[])
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title:'any',
+      url:'www.any.com',
+      techs:['node']
+    })
+    setRepositories([ ...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      {repositories.map(repo => {
+        return (
+          <li key={repo.id}>
+          {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
-        </li>
+          </li>
+        )
+      })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
